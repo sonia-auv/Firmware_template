b6a63d58fb8c5fc6e2c6aa98ec25e77d7478d0cc
Lucas Mongrain <xeaprog@gmail.com>
2019-10-06 17:57:44 -0400

    add RS485 pin definition

--- include/pinDef.h
+++ include/pinDef.h
@@ -1,4 +1,11 @@
 #ifndef PIN_DEF_H
 #define PIN_DEF_H
 
+//###################################################
+//              RS485 PIN DEFINITION
+//###################################################
+
+#define RS485_RX_PIN PA_10
+#define RS485_TX_PIN PA_9
+
 #endif
